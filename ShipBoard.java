3237cae715a1e809685cf2cdd127957c7dbd22a0
RitsuCloud <102085854+RitsuCloud@users.noreply.github.com>
2022-03-23 17:48:12 -0700

    Add files via upload

---
+++ ShipBoard.java
@@ -0,0 +1,156 @@
+//Chon Hin Chou
+//Project #1 Single player battleship
+//
+//This class is class that builds the board and prints out
+//the board in the console, it tracks which spot has been hit 
+//and display if it his a battleship
+//Counts from top to bottom. left to right, so top left corner is (0, 0)
+//----------------------------------------------------------
+//Needs:
+//add method for randomize adding ships
+//add method for get number of ships left
+
+import java.util.*;
+
+public class ShipBoard{
+   
+   private int size;
+   private Map<Integer, List<Integer>> board;
+   private Map<Integer, List<Integer>> storage;  //Store the location of the ships
+   private int guess;
+   
+   //Pre: 10 >= size >= 4 (throw IllegalArgumentException if not)
+   //Post: Constructor for the class, construct a board based on given size, and amount of guesses
+
+   public ShipBoard(int size, int guess){
+      if(size < 4 || size > 10){
+         throw new IllegalArgumentException();
+      }
+      this.size = size;
+      this.guess = guess;
+      board = new TreeMap<>();
+      storage = new TreeMap<>();
+      for(int i = 0; i < size; i++){
+         List<Integer> tempt = new ArrayList<>();
+         for(int j = 0; j < size; j++){
+            tempt.add(0);
+         }
+         board.put(i, tempt);
+      }
+   }
+   
+   //Post: Prints out the current board
+   public void printBoard(){
+      System.out.print("  ");
+      for(int i = 0; i < size; i++){
+         System.out.print(i + " ");
+      }
+      System.out.println();
+      for(int n: board.keySet()){
+         List<Integer> tempt = board.get(n);
+         System.out.print(n + " ");
+         for(int i = 0; i < tempt.size(); i++){
+            if(tempt.get(i) < 0){   // if the value is less than 0, that means it hit an ship
+               System.out.print("X ");   
+            }else if(tempt.get(i) > 0){  // if the value is bigger than 10, means it hit nothing
+               System.out.print("  ");
+            }else{   // else than the spot is untoubled
+               System.out.print("- ");
+            }
+         }
+         System.out.println();
+      }
+      System.out.println();
+   }
+   
+   //Pre: size >= x > 0, size >= y > 0
+   //(throw new IllegalArgumentException if not)
+   //Post: add an battle ship to the board
+   public void add(int x, int y){
+      if(x > size || y > size || x <= 0 || y <=0){
+         throw new IllegalArgumentException();
+      }
+      if(storage.containsKey(x)){
+         List<Integer> tempt = storage.get(x);;
+         tempt.add(y);
+      }else{
+         List<Integer> tempt = new ArrayList<>();
+         tempt.add(y);
+         storage.put(x,tempt);
+      }
+   }
+   
+   //Pre: size >= x > 0, size >= y > 0
+   //(throw new IllegalArgumentException if not)
+   //Post: strike the row and colum, and guess -1 if nothing is hit
+   public void hit(int x, int y){
+      if(x > size - 1 || y > size - 1 || x < 0 || y <0){
+         throw new IllegalArgumentException();
+      }
+      if(storage.containsKey(x)){  //if there is a ship on the same row
+         List<Integer> tempt = board.get(x);
+         List<Integer> tempt2 = storage.get(x);
+         if(tempt2.contains(y)){  //if there is a ship on the spot
+            tempt.set(y, -10);
+         }else{
+            tempt.set(y, 10);
+            guess--;  //guess -1 since they didnt get anything
+         }
+      }else{
+         List<Integer> tempt = board.get(x);  //when there is no ship on the row
+         tempt.set(y, 10);
+         guess--;   //guess -1 since they didnt get anything
+      }
+   }
+   
+   //Pre: amount of ships <= size*size
+   //(throw IllegalArgumentException if not)
+   //Post: randomize ship placement based on how many ship the user wants
+   public void randomize (int amount){
+      if(amount > (size * size)){
+         throw new IllegalArgumentException();
+      }
+      Random rand = new Random();
+      for(int i = 0; i < amount; i++){
+         int x = rand.nextInt(size);
+         int y = rand.nextInt(size);
+         if(storage.containsKey(x)){
+            List<Integer> tempt = storage.get(x);
+            while(tempt.size() >= size){
+               x = rand.nextInt(size);
+               tempt = storage.get(x);
+            }
+            while(tempt.contains(y)){
+               y = rand.nextInt(size);
+            }
+            tempt.add(y);
+         }else{
+            List<Integer> tempt = new ArrayList<>();
+            tempt.add(y);
+            storage.put(x, tempt);
+         }
+      }
+   }
+   
+   //Post: method returns the row where it contains 1 or more ships
+   public int hint(){
+      Set<Integer> keySet = storage.keySet();
+      Iterator<Integer> i = keySet.iterator();
+      return i.next();
+   }
+   //Post: this method returns true when the user has run out of guesses
+   //or they hit all the ships
+   public boolean gameOver(){ 
+      return guess<=0 || storage.isEmpty();
+   }
+   
+   //Post: returns the number of guess left
+   public int getGuess(){
+      return guess;
+   }
+   
+   //Post: return true if all the ships been hit
+   public boolean getShips(){
+      return storage.isEmpty();
+   }
+}
---
+++ client.java
@@ -0,0 +1,53 @@
+//Chon Hin Chou
+//Project #1 Single player battleship
+//
+//This is the main client of the program where user can play the game. 
+//This is responsible for building ShipBoard and many other user interaction
+//like giving corrdinate to hit and run the game
+import java.util.*;
+
+public class client{
+   
+   //Post: the main method that is responsible for running the game
+   public static void main(String[] args){
+      Scanner console = new Scanner(System.in);
+      intro();
+      int size = console.nextInt();
+      System.out.print("Please enter the amount of guesses you want to have! (size you pick * 2) would be optimal : ");
+      int guess = console.nextInt();
+      System.out.print("Please enter the amount of ships you wish to have on the board! : ");
+      int amount = console.nextInt();
+      ShipBoard board = new ShipBoard(size,guess);
+      board.randomize(amount);
+      while(!board.gameOver()){
+         System.out.println();
+         board.printBoard();
+         System.out.println("You have " + board.getGuess() + " guesses left!");
+         System.out.print("Please enter x and y corrdinate you wish to strike! Enter in the form x y (Type HINT for help): ");
+         String answer = console.next();
+         if(answer.equals("HINT")){
+            System.out.println("There is a ship on row " + board.hint());
+         }else{
+            int x = Integer.parseInt(answer);
+            int y = console.nextInt();
+            board.hit(x,y);
+         }
+      }
+      board.printBoard();
+      if(board.getGuess() == 0){
+         System.out.println("Sorry, you have ran out of guesses, maybe give yourself more guesses since this is too hard for you!!");
+      }else{
+         System.out.println("YOU WON!!!!, CONGRATULATION!!!, NOW TIME TO PICK A BIGGER BOARD WITH LESS GUESSES TO CHALLENGE YOURSELF!!");
+      }
+   }
+   
+   //Post: prints out the introduction and rule of the game 
+   public static void intro(){
+      System.out.println("Welcome to the overly simplified single player battleship!!!");
+      System.out.println("Spot striked that were empty will appear empty, spot striked that has ship will appear as X!!");
+      System.out.println("Follow the given instruction and don't give random responses!!!");
+      System.out.println("Have fun and enjoy the game!!!");
+      System.out.println();
+      System.out.print("Please enter a size for the board! (4 - 6) would be optimal, Don't exceed 10! : ");
+   }
+}
